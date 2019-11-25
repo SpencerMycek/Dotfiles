72426298e57ef6afc362f3b2d9bbd0178190559f
Spencer Mycek <Spence.Mycek@gmail.com>
2019-11-25 09:44:43 -0500

    Add configs for swaylock

--- Arch/config/waybar/modules/rotate-left.sh
+++ Arch/config/waybar/modules/rotate-left.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Rotates the identified output
-swaymsg "output 'Unknown 0x06AC 0x00000000' transform 0" 
+#swaymsg 'input "1386:20554:Wacom_Pen_and_multitouch_sensor_Finger" calibration_matrix 1 0 1 0 1 1'
+#swaymsg 'input "1386:20554:Wacom_Pen_and_multitouch_sensor_Pen" calibration_matrix 1 0 1 0 1 1'
+#swaymsg "output 'Unknown 0x06AC 0x00000000' transform 0" 
 
--- Arch/config/waybar/modules/rotate-right.sh
+++ Arch/config/waybar/modules/rotate-right.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 # Rotates the identified output
-swaymsg "output 'Unknown 0x06AC 0x00000000' transform 90" 
+cos=0
+sin=1
+negsin=-${sin}
+xoff=1
+yoff=1
+pen_command='input "1386:20554:Wacom_Pen_and_multitouch_sensor_Pen" '
+finger_command='input "1386:20554:Wacom_Pen_and_multitouch_sensor_Finger" '
+input_command='calibration_matrix '$cos' '$negsin' '$xoff' '$sin' '$cos' '$yoff
+#echo $pen_command $input_command
 
+#swaymsg "output 'Unknown 0x06AC 0x00000000' transform 90" 
+#swaymsg 'input "1386:20554:Wacom_Pen_and_multitouch_sensor_Pen" calibration_matrix 0 -1 1 1 0 1'
+#swaymsg 'input "1386:20554:Wacom_Pen_and_multitouch_sensor_Finger" calibration_matrix 0 -1 1 1 0 1'
